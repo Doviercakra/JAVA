371662c12d15ce4e1538e1d1dd9f4173d7b86705
Doviercakra <39761370+Doviercakra@users.noreply.github.com>
2018-05-30 07:32:01 -0700

    Add files via upload

---
+++ Perkalian8.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perkalian8;
+
+/**
+ *
+ * @author user
+ */
+public class Perkalian8 {
+
+    public static void main(String[] args) {
+        System.out.println("Dovier Cakrawardaya");
+        System.out.println("E1E117055");
+        System.out.println(" ");
+        
+        System.out.println("Membuat perkalian 8 menggunakan looping");
+        int nilai= 0;
+        for (int i=1;i<11;i++){
+            nilai = i*8;
+        System.out.println(i + " X 8 = " + nilai);
+    }
+    
+}
+}
